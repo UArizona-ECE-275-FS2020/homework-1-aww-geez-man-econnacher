95e66e2ebce1950d2cb60599d2fbe628e0b2b6bd
econnacher <70343514+econnacher@users.noreply.github.com>
2020-09-26 15:16:23 -0700

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -13,9 +13,58 @@
 // =========================================================================== // 
 //          Define functions enclosed in Namespace C137 here 
 // =========================================================================== // 
+void C137::Morty(int start, int stop) {
+   for(int i = start; i <= stop; i++){
+       std::cout << i << ": ";
+       if(i%15 == 0)
+           std::cout << "Aww Geez Man";
+        else if(i%5 == 0)
+           std::cout << "Geez";
+        else if(i%3 == 0)
+           std::cout << "Aww";
+        std::cout << "" << std::endl;
+   }
+}
 
+void C137::Morty(int start, int stop, int step) {
+    for(int i = start; i <= stop; i = i + step){
+       std::cout << i << ": ";
+       if(i%15 == 0)
+           std::cout << "Aww Geez Man";
+        else if(i%5 == 0)
+           std::cout << "Geez";
+        else if(i%3 == 0)
+           std::cout << "Aww";
+        std::cout << "" << std::endl;
+   }
+}
 
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
+void Z286::Morty(int start, int stop) {
+     for(int i = start; i <= stop; i++){
+       std::cout << i << ": ";
+       if(i%15 == 0)
+           std::cout << "naM zeeG wwA";
+        else if(i%5 == 0)
+           std::cout << "zeeG";
+        else if(i%3 == 0)
+           std::cout << "wwA";
+        std::cout << "" << std::endl;
+   }
+}
+
+void Z286::Morty(int start, int stop, int step) {
+    for(int i = start; i <= stop; i = i + step){
+       std::cout << i << ": ";
+       if(i%15 == 0)
+           std::cout << "naM zeeG wwA";
+        else if(i%5 == 0)
+           std::cout << "zeeG";
+        else if(i%3 == 0)
+           std::cout << "wwA";
+        std::cout << "" << std::endl;
+   }
+}
